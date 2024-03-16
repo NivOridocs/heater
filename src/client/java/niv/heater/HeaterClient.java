caff5cfcc8088f569c5323de549e93e0766882c6
Niv Oridocs <fradige95@gmail.com>
2024-03-16 16:39:01 +0100

    Fix client-side

--- src/client/java/niv/heater/HeaterClient.java
+++ src/client/java/niv/heater/HeaterClient.java
@@ -1,15 +1,15 @@
 package niv.heater;
 
-import static niv.heater.Heater.HEATER_SCREEN_HANDLER;
+import static niv.heater.Heater.HEATER_MENU_TYPE;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.gui.screens.MenuScreens;
 
 public class HeaterClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HandledScreens.register(HEATER_SCREEN_HANDLER, HeaterScreen::new);
+        MenuScreens.register(HEATER_MENU_TYPE, HeaterScreen::new);
     }
 
 }
