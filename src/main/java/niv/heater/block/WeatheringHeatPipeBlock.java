fbebbd3cc274ccedd663f605a85d01883ca08547
Niv Oridocs <fradige95@gmail.com>
2024-03-17 16:10:18 +0100

    Add codecs

--- src/main/java/niv/heater/block/WeatheringHeatPipeBlock.java
+++ src/main/java/niv/heater/block/WeatheringHeatPipeBlock.java
@@ -1,20 +1,37 @@
 package niv.heater.block;
 
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.ChangeOverTimeBlock;
 import net.minecraft.world.level.block.WeatheringCopper;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class WeatheringHeatPipeBlock extends HeatPipeBlock implements WeatheringCopper {
+
+    @SuppressWarnings("java:S1845")
+    public static final MapCodec<WeatheringHeatPipeBlock> CODEC = RecordCodecBuilder.mapCodec(instance -> instance
+            .group(
+                    WeatherState.CODEC.fieldOf("weathering_state").forGetter(ChangeOverTimeBlock::getAge),
+                    Properties.CODEC.fieldOf("properties").forGetter(BlockBehaviour::properties))
+            .apply(instance, WeatheringHeatPipeBlock::new));
 
     public WeatheringHeatPipeBlock(WeatherState weatherState, Properties settings) {
         super(weatherState, settings);
     }
 
     @Override
+    public MapCodec<? extends WeatheringHeatPipeBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
     public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        this.onRandomTick(state, level, pos, random);
+        this.changeOverTime(state, level, pos, random);
     }
 
     @Override
