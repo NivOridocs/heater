ecc9f5d79cb6a583c8c4e229fc8b61becf3cc943
Niv Oridocs <fradige95@gmail.com>
2024-10-13 12:26:42 +0200

    Update README

--- README.sh
+++ README.sh
@@ -7,4 +7,5 @@ cat README.md \
         s_img/Heater\_Recipe\.png_https://cdn.modrinth.com/data/g3jBz897/images/274da1bd3a20ff4bbbb441f71f9c8ce74a47a1d9.png_;
         s_img/Heat\_Pipe\_Recipe\.png_https://cdn.modrinth.com/data/g3jBz897/images/a86e1322ec5c0273ea234dd842cb5ec8eb97ecf2.png_;
         s_img/Thermostat\_Recipe\.png_https://cdn.modrinth.com/data/g3jBz897/images/af7fbab17256c8b32aec0d1b3d64cd9fb0749425.png_;
-        s_https://github.com/0Starocean0_https://modrinth.com/user/StarOcean_;'
+        s_https://github.com/0Starocean0_https://modrinth.com/user/StarOcean_;
+        s_https://github.com/NivOridocs/burning_https://modrinth.com/mod/burning_;'
